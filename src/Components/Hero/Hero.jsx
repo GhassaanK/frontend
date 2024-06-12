dc3cde67f3945dc19c27a92d1ee18aee17666aef
Ghassaan <ghassaank1@gmail.com>
2024-06-12 14:25:54 +0500

    Another Commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import arrow_icon from '../Assets/arrow.png'
+import hero_img from '../Assets/hero_image.png'
+import hand_icon from '../Assets/hand_icon.png'
 export const Hero = () => {
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>New Arrivals</h2>
+            <div className="">
+                <div className="hero-hand-icon">
+                    <p>New</p>
+                    <img src={hand_icon} alt="Hand" />
+                </div>
+                <p>Collections</p>
+                <p>for everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt="Arrow Icon" />
+            </div>
+        </div>
+        <div className="hero-right">
+            <img src={hero_img} alt="Hero Image" />
+        </div>
+    </div>
   )
 }
